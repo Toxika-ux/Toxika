65d191b789a366b9245024c575eb6530c00dadf0
Toxika-ux <73974748+Toxika-ux@users.noreply.github.com>
2020-11-04 23:20:49 -0300

    Update main.java

--- DiscordBotYes/Toxika/src/main/main.java
+++ DiscordBotYes/Toxika/src/main/main.java
@@ -9,15 +9,16 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class main 
 {
+	//declarando variaveis importantes
 	public static JDA jda;
 	public static String prefix = "!";
 	  public static void main(String[] args) throws LoginException
 	  {
 	         @SuppressWarnings("unused")
-			 JDA builder = JDABuilder.createDefault("NzczNTE0MzE3MDQ3Mzk4NDYw.X6KVWA.tYR7VBgiN5JmyQc2kF4czX1IY10")
-	         .setActivity(Activity.playing("a"))
-	         .addEventListeners(new Commands())
-	         .addEventListeners(new Clear())
-	         .build();	        
+		 JDA builder = JDABuilder.createDefault("NzczNTE0MzE3MDQ3Mzk4NDYw.X6KVWA.tYR7VBgiN5JmyQc2kF4czX1IY10") //Aqui vem o token do bot para controla-lo esse não é o token que o bot usa atualmente
+	         .setActivity(Activity.playing("a")) //Atualizar a "atividade" do bot para Jogando: alguma-coisa
+	         .addEventListeners(new Commands())  //Aqui vem os eventListeners...
+	         .addEventListeners(new Clear())     //...
+	         .build();	//fim	        
 	   }
 	}
