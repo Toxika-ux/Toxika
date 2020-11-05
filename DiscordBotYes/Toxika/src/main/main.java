9b82e2148cd3d4f33309a84595224d790fff0ee4
Toxika-ux <73974748+Toxika-ux@users.noreply.github.com>
2020-11-04 23:40:58 -0300

    Adicionar comentarios para Main.java

--- DiscordBotYes/Toxika/src/main/main.java
+++ DiscordBotYes/Toxika/src/main/main.java
@@ -1,3 +1,8 @@
+/*
+* principal arquivo, aqui eu controlo meu bot de forma geral
+* também uso esse arquivo para declarar variáveis globais "public static JDA jda;" é um exemplo
+*/
+
 package main;
 
 import javax.security.auth.login.LoginException;
@@ -14,7 +19,7 @@ public class main
 	  public static void main(String[] args) throws LoginException
 	  {
 	         @SuppressWarnings("unused")
-		 JDA builder = JDABuilder.createDefault("NzczNTE0MzE3MDQ3Mzk4NDYw.X6KVWA.tYR7VBgiN5JmyQc2kF4czX1IY10")
+		 JDA builder = JDABuilder.createDefault("NzczNTE0MzE3MDQ3Mzk4NDYw.X6KVWA.tYR7VBgiN5JmyQc2kF4czX1IY10")	// Não, esse não é o meu token real.
 	         .setActivity(Activity.playing("a"))
 	         .addEventListeners(new Commands())
 	         .addEventListeners(new Clear())
